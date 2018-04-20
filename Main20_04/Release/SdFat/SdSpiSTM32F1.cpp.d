6bff48fedc53312ac464e38067a094585a5600d2
promavto <promavto@ntmp.ru>
2018-04-20 11:06:06 +0300

    Main20_04

---
+++ Main20_04/Release/SdFat/SdSpiSTM32F1.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\Main20_04\arduino_due_x_dbg\Release\SdFat\SdSpiSTM32F1.cpp.o: \
+ D:\2\ Git\ Teplica\Main20_04\libraries\SdFat\SdSpiSTM32F1.cpp
---
+++ Main20_04/Release/UTFT/DefaultFonts.c.d
@@ -0,0 +1,2 @@
+C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\Main20_04\arduino_due_x_dbg\Release\UTFT\DefaultFonts.c.o: \
+ D:\2\ Git\ Teplica\Main20_04\libraries\UTFT\DefaultFonts.c
