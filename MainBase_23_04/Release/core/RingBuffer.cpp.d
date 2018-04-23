6bc0bee56a6300019108c60d3281e9f71ec5d980
promavto <promavto@ntmp.ru>
2018-04-23 12:47:04 +0300

    MainBase_23_04.bin

---
+++ MainBase_23_04/Release/core/RingBuffer.cpp.d
@@ -0,0 +1,3 @@
+C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\MainBase_23_04\arduino_due_x_dbg\Release\core\RingBuffer.cpp.o: \
+ C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.11\cores\arduino\RingBuffer.cpp \
+ C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.11\cores\arduino\RingBuffer.h
