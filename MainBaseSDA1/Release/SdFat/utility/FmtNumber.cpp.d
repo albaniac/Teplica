66ef4d6da0857c2b5f52e67b73288c861b01b14b
promavto <promavto@ntmp.ru>
2018-05-14 12:19:26 +0300

    с

---
+++ MainBaseSDA1/Release/SdFat/utility/FmtNumber.cpp.d
@@ -0,0 +1,3 @@
+C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\MainBaseSDA1\arduino_due_x_dbg\Release\SdFat\utility\FmtNumber.cpp.o: \
+ D:\2\ Git\ Teplica\MainBaseSDA1\libraries\SdFat\utility\FmtNumber.cpp \
+ D:\2\ Git\ Teplica\MainBaseSDA1\libraries\SdFat\utility\FmtNumber.h
---
+++ MainBaseSDA1/Release/core/wiring_pulse_asm.S.d
@@ -0,0 +1,2 @@
+C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\MainBaseSDA1\arduino_due_x_dbg\Release\core\wiring_pulse_asm.S.o: \
+ C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.11\cores\arduino\wiring_pulse_asm.S
