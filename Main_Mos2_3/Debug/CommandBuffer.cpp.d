bb72da2573b8720270e24cde664c8bb36c7943aa
promavto <promavto@ntmp.ru>
2018-05-14 20:08:24 +0300

    v

---
+++ Main_Mos2_3/Debug/CommandBuffer.cpp.d
@@ -0,0 +1,12 @@
+C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_Mos2_3\arduino_due_x_dbg\Debug\CommandBuffer.cpp.o: \
+ C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_Mos2_3\arduino_due_x_dbg\Debug\CommandBuffer.cpp \
+ C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_Mos2_3\arduino_due_x_dbg\Debug\CommandBuffer.h \
+ C:\Users\Alex\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.11\cores\arduino/Stream.h \
+ C:\Users\Alex\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.11\cores\arduino/Print.h \
+ C:\Users\Alex\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.11\cores\arduino/WString.h \
+ C:\Users\Alex\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.11\cores\arduino/avr/pgmspace.h \
+ C:\Users\Alex\AppData\Local\arduino15\packages\arduino\hardware\sam\1.6.11\cores\arduino/Printable.h \
+ C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_Mos2_3\arduino_due_x_dbg\Debug\Globals.h \
+ C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_Mos2_3\arduino_due_x_dbg\Debug\Configuration_DUE.h \
+ C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_Mos2_3\arduino_due_x_dbg\Debug\Configuration_DEBUG.h \
+ C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_Mos2_3\arduino_due_x_dbg\Debug\Configuration_Shared.h
