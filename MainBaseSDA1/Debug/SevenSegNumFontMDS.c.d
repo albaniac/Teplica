b1cdaa5446e53d5db34b0bb485a83f3450df4e16
promavto <promavto@ntmp.ru>
2018-05-23 21:01:51 +0300

    c

---
+++ MainBaseSDA1/Debug/SevenSegNumFontMDS.c.d
@@ -0,0 +1,2 @@
+C:\Users\Alex\AppData\Local\Temp\VMBuilds\MainBaseSDA1\arduino_due_x_dbg\Debug\SevenSegNumFontMDS.c.o: \
+ C:\Users\Alex\AppData\Local\Temp\VMBuilds\MainBaseSDA1\arduino_due_x_dbg\Debug\SevenSegNumFontMDS.c
