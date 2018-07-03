3778520a58dae045e8e6be1c5818fd73fdab96c2
promavto <promavto@ntmp.ru>
2018-07-03 23:04:52 +0300

    с

--- Main_Mos_06_24/Release/tft_water_button.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_Mos_06_24\arduino_due_x_dbg\Release\tft_water_button.c.o: \
- C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_Mos_06_24\arduino_due_x_dbg\Release\tft_water_button.c
