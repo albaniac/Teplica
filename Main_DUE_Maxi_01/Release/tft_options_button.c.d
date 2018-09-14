1520c87b8dc4dee1cf6e972b98abda90995540b9
promavto <promavto@ntmp.ru>
2018-09-14 15:45:32 +0300

    а

---
+++ Main_DUE_Maxi_01/Release/tft_options_button.c.d
@@ -0,0 +1,2 @@
+C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\Main_DUE_Maxi_01\arduino_due_x_dbg\Release\tft_options_button.c.o: \
+ C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\Main_DUE_Maxi_01\arduino_due_x_dbg\Release\tft_options_button.c
