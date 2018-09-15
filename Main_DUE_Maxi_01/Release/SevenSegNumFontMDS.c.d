c27210084f6165d536b9389d6a013420abeccaf1
promavto <promavto@ntmp.ru>
2018-09-15 07:16:29 +0300

    DS3231

--- Main_DUE_Maxi_01/Release/SevenSegNumFontMDS.c.d
+++ Main_DUE_Maxi_01/Release/SevenSegNumFontMDS.c.d
@@ -1,2 +1,2 @@
-C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\Main_DUE_Maxi_01\arduino_due_x_dbg\Release\SevenSegNumFontMDS.c.o: \
- C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\Main_DUE_Maxi_01\arduino_due_x_dbg\Release\SevenSegNumFontMDS.c
+C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_DUE_Maxi_01\arduino_due_x_dbg\Release\SevenSegNumFontMDS.c.o: \
+ C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_DUE_Maxi_01\arduino_due_x_dbg\Release\SevenSegNumFontMDS.c
