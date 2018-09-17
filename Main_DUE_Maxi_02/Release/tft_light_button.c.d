9291546f7280f38bcdfec3523f54764de51147e1
promavto <promavto@ntmp.ru>
2018-09-17 07:59:39 +0300

    f

---
+++ Main_DUE_Maxi_02/Release/tft_light_button.c.d
@@ -0,0 +1,2 @@
+C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_DUE_Maxi_02\arduino_due_x_dbg\Release\tft_light_button.c.o: \
+ C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_DUE_Maxi_02\arduino_due_x_dbg\Release\tft_light_button.c
