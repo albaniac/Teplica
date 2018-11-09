fda8a7ede13679a2e9bc900e33d6ae276aa52991
promavto <promavto@ntmp.ru>
2018-11-10 02:50:07 +0300

    f

---
+++ Main_11_07/Release/SdFat/SdSpiSTM32F1.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_11_07\arduino_due_x_dbg\Release\SdFat\SdSpiSTM32F1.cpp.o: \
+ D:\2\ GIT\ Teplica\Main_11_07\libraries\SdFat\SdSpiSTM32F1.cpp
---
+++ Main_11_07/Release/SevenSegNumFontMDS.c.d
@@ -0,0 +1,2 @@
+C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_11_07\arduino_due_x_dbg\Release\SevenSegNumFontMDS.c.o: \
+ C:\Users\Alex\AppData\Local\Temp\VMBuilds\Main_11_07\arduino_due_x_dbg\Release\SevenSegNumFontMDS.c
